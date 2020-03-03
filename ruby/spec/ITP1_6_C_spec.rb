329491eedc2ee69f7932c8dbdc2327b9be208abf
kawamataryou <ba068082@gmail.com>
2020-03-04 06:26:38 +0900

    [from now] 2020/03/04 06:26:38
    diff --git a/ruby/spec/ITP1_6_C_spec.rb b/ruby/spec/ITP1_6_C_spec.rb
    index fbd808b..eefd22a 100644
    --- a/ruby/spec/ITP1_6_C_spec.rb
    +++ b/ruby/spec/ITP1_6_C_spec.rb
    @@ -13,36 +13,41 @@ class Building
    
       def print_move_in_status
         rooms.each do |f|
    -      puts f.join(' ')
    +      puts ' ' + f.join(' ')
         end
       end
     end
    
    -def func; end
    -
    -describe 'sample' do
    -  let(:stdin) do
    -    <<~EOF
    -      8
    -      3 3 4 4 5 8 7 9
    -    EOF
    -  end
    -
    -  let(:result) do
    -    <<~EOF
    -      9 7 8 5 4 4 3 3
    -    EOF
    -  end
    -
    -  before do
    -    allow(ARGF).to receive(:gets) { stdin }
    -    $stdout = StringIO.new
    +def func
    +  n = gets.to_i
    +  move_in_histories = n.times.map{gets.split.map(&:to_i)}
    +  building_a = Building.new
    +  building_b = Building.new
    +  building_c = Building.new
    +  building_d = Building.new
    +
    +  move_in_histories.each do |history|
    +    b, f, r, v = history
    +    case b
    +    when 1
    +      building_a.move_in(f, r, v)
    +    when 2
    +      building_b.move_in(f, r, v)
    +    when 3
    +      building_c.move_in(f, r, v)
    +    when 4
    +      building_d.move_in(f, r, v)
    +    else
    +      # type code here
    +    end
       end
    
    -  context '' do
    -    it {
    -      func
    -      expect($stdout.string).to eq result
    -    }
    -  end
    +  puts building_a.print_move_in_status
    +  puts '#' * 20
    +  puts building_b.print_move_in_status
    +  puts '#' * 20
    +  puts building_c.print_move_in_status
    +  puts '#' * 20
    +  puts building_d.print_move_in_status
     end
    +

--- ruby/spec/ITP1_6_C_spec.rb
+++ ruby/spec/ITP1_6_C_spec.rb
@@ -13,36 +13,41 @@ class Building
 
   def print_move_in_status
     rooms.each do |f|
-      puts f.join(' ')
+      puts ' ' + f.join(' ')
     end
   end
 end
 
-def func; end
+def func
+  n = gets.to_i
+  move_in_histories = n.times.map{gets.split.map(&:to_i)}
+  building_a = Building.new
+  building_b = Building.new
+  building_c = Building.new
+  building_d = Building.new
 
-describe 'sample' do
-  let(:stdin) do
-    <<~EOF
-      8
-      3 3 4 4 5 8 7 9
-    EOF
+  move_in_histories.each do |history|
+    b, f, r, v = history
+    case b
+    when 1
+      building_a.move_in(f, r, v)
+    when 2
+      building_b.move_in(f, r, v)
+    when 3
+      building_c.move_in(f, r, v)
+    when 4
+      building_d.move_in(f, r, v)
+    else
+      # type code here
+    end
   end
 
-  let(:result) do
-    <<~EOF
-      9 7 8 5 4 4 3 3
-    EOF
-  end
-
-  before do
-    allow(ARGF).to receive(:gets) { stdin }
-    $stdout = StringIO.new
-  end
-
-  context '' do
-    it {
-      func
-      expect($stdout.string).to eq result
-    }
-  end
+  puts building_a.print_move_in_status
+  puts '#' * 20
+  puts building_b.print_move_in_status
+  puts '#' * 20
+  puts building_c.print_move_in_status
+  puts '#' * 20
+  puts building_d.print_move_in_status
 end
+
